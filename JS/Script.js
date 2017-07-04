9c8d2b59210d8cf1a7c836b10aff9f37ccebf015
Giovanni Rodrigues <giovannirodrigues.2012@gmail.com>
2017-07-04 18:11:30 -0300

    Menu hamburguer
    foi adicionado o menu hamburguer

--- JS/Script.js
+++ JS/Script.js
@@ -1,5 +1,5 @@
-$(function (){
-    $("#slides ul").cycle ({
+$(function () {
+    $("#slides ul").cycle({
         fx: 'fade',
         speed: 2000,
         timeout: 4000,
@@ -7,15 +7,24 @@ $(function (){
         next: '#next',
     })
 });
-$(window).on("scroll", function() {
+
+$(window).on("scroll", function () {
     var top = $(window).scrollTop();
     var height = $("#menu").height();
 
     if (top >= (height - 50)) {
-        $("#logo").css({"width" : "35px"});
-        $("#menu").css({"box-shadow" : "0.5px 2px 4px rgba(0, 0, 0, 0.3), 0.5px 4px 8px rgba(0, 0, 0, 0.1)"});
+        $("#logo").css({ "width": "35px" });
+        $("#menu").css({ "box-shadow": "0.5px 2px 4px rgba(0, 0, 0, 0.3), 0.5px 4px 8px rgba(0, 0, 0, 0.1)" });
     } else {
-        $("#logo").css({"width" : "100px"});
-        $("#menu").css({"box-shadow" : "none"});
+        $("#logo").css({ "width": "100px" });
+        $("#menu").css({ "box-shadow": "none" });
     }
+});
+
+$(".hambg").on("click", function () {
+    $("#menuh").css({ "opacity": "1", "margin-left": "0" });
+});
+
+$(".sair").on("click", function () {
+    $("#menuh").css({ "opacity": "0", "margin-left": "-320px" });
 });
