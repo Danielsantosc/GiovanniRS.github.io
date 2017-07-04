f6b145cc4c096586eb9efc971b9c3adfd8fa9e46
Giovanni Rodrigues <giovannirodrigues.2012@gmail.com>
2017-07-04 18:16:24 -0300

    atualização de bugs
    atualização do menu hamburguer para a correção de erros

--- JS/Script.js
+++ JS/Script.js
@@ -10,9 +10,8 @@ $(function () {
 
 $(window).on("scroll", function () {
     var top = $(window).scrollTop();
-    var height = $("#menu").height();
 
-    if (top >= (height - 50)) {
+    if (top != 0) {
         $("#logo").css({ "width": "35px" });
         $("#menu").css({ "box-shadow": "0.5px 2px 4px rgba(0, 0, 0, 0.3), 0.5px 4px 8px rgba(0, 0, 0, 0.1)" });
     } else {
